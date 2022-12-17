15d41fa93860692e5034d29ab88dda5735b6255d
dayi <yumibiao10@gmail.com>
2022-12-17 21:11:15 +0800

    22年12月17日21:11:15

--- auto_push.py
+++ auto_push.py
@@ -2,7 +2,7 @@ import os
 os.system("git add .")
 import datetime
 dt = datetime.datetime.now()
-dt_str = dt.strftime("%y年%m月%d天-%H:%M:%S")
+dt_str = dt.strftime("%y年%m月%d日%H:%M:%S")
 
 os2 = 'git commit -m "{}" '.format(dt_str)
 print(os2)
