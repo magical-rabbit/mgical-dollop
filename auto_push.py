7c2851cfab9512364ccce4e99b4839f6a9cad701
dayi <yumibiao10@gmail.com>
2022-12-18 00:27:58 +0800

    22年12月18日00:27:58 删除无关文件

--- auto_push.py
+++ auto_push.py
@@ -3,7 +3,7 @@ os.system("git pull")
 os.system("git add .")
 import datetime
 dt = datetime.datetime.now()
-dt_str = dt.strftime("%y年%m月%d日%H:%M:%S 要闻传真的首页忘了获取")
+dt_str = dt.strftime("%y年%m月%d日%H:%M:%S 删除无关文件")
 
 os2 = 'git commit -m "{}" '.format(dt_str)
 print(os2)
