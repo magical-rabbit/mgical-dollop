26b1a8aef52cfd491dbc377b1cc9fd98fc17ac1d
dayi <yumibiao10@gmail.com>
2022-12-18 00:07:11 +0800

    22年12月18日00:07:11 更改gitignore

--- auto_push.py
+++ auto_push.py
@@ -1,8 +1,9 @@
 import os
+os.system("git pull")
 os.system("git add .")
 import datetime
 dt = datetime.datetime.now()
-dt_str = dt.strftime("%y年%m月%d日%H:%M:%S 获得标题，日期格式化为datetime类型，获得点击量")
+dt_str = dt.strftime("%y年%m月%d日%H:%M:%S 更改gitignore")
 
 os2 = 'git commit -m "{}" '.format(dt_str)
 print(os2)
