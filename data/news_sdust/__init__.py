5dc937302e789a82432e1e46687a80f5e4004090
dayi <yumibiao10@gmail.com>
2022-12-18 16:10:10 +0800

    22年12月18日16:10:10 新闻页面的接口进行打包

---
+++ data/news_sdust/__init__.py
@@ -0,0 +1,7 @@
+# 直接写成模块吧
+
+from data.news_sdust.get_list_ywcz import *
+from data.news_sdust.get_pages import *
+from data.news_sdust.download_pic import *
+# from get_title_ta import * 没有封装成函数
+from data.news_sdust.get_title import *
---
+++ insert_fax_news.py
@@ -0,0 +1,14 @@
+#要闻传真，嘿嘿嘿，总算写完了全部的接口了，开整！
+#因为几乎所有的接口都已经写了，这里甚至不需要手动进行处理什么东西
+import lib.dayi_sqlite as dayidb #导入数据库
+db = dayidb.db() #初始化数据库，已经都自动初始化啦
+
+from data.news_sdust import * #爬虫相关的库
+
+
+def main():
+  list_news = get_all_news()
+  print(list_news)
+  return
+
+main()
